28461cd2c283edb34b05b3af5d6383b3ce31eb31
StrangeresAnn <lykova_annushka@mail.ru>
2023-04-09 18:17:10 +0400

    Task6

---
+++ src/homework/util/impl/UserSaver.java
@@ -0,0 +1,10 @@
+package homework.util.impl;
+
+import homework.User;
+
+public class UserSaver {
+    public static void save(User user) {
+        System.out.println("Save user: " + user.getName());
+    }
+}
+
