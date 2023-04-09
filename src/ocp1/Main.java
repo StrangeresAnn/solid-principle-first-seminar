15623190b084d2e7e7e11e2b5109f7901fb3a41f
StrangeresAnn <lykova_annushka@mail.ru>
2023-04-09 18:15:03 +0400

    Task6

--- src/ocp1/Main.java
+++ src/ocp1/Main.java
@@ -1,5 +1,6 @@
 package ocp1;
 
+
 import ocp1.shape.Shape;
 import ocp1.shape.impl.RightTriangle;
 import ocp1.shape.impl.Square;
@@ -14,14 +15,7 @@ public class Main {
         shapes.add(new Square(4));
         double sumArea = 0;
         for (Shape shape : shapes) {
-            if (shape instanceof RightTriangle) {
-                RightTriangle triangle = (RightTriangle) shape;
-                sumArea += triangle.getLeg1() * triangle.getLeg2() / 2.0;
-            }
-            if (shape instanceof Square) {
-                Square square = (Square) shape;
-                sumArea += Math.pow(square.getSide(), 2);
-            }
+            sumArea += shape.getArea();
         }
 
         System.out.printf("Сумма площадей фигур равна %f \n", sumArea);
